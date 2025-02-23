28f0906baeadbd5a708626c68d768ec7e7c851e5
BI1t <98274631+BI1t@users.noreply.github.com>
2025-02-23 19:00:48 -0300

    Update Bit.js

--- Bit.js
+++ Bit.js
@@ -9,7 +9,7 @@
       "title": "Elden Ring.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
     },
-     {
+    {
       "fileSize": "1.35 GB",
       "uris": [
         "https://1fichier.com/?lfpsxc964sq3dcnc4m76"
@@ -17,13 +17,13 @@
       "title": "Perfect Heist 2.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
         },
-         {
+        {
       "fileSize": "624.88 MB",
       "uris": [
         "https://1fichier.com/?n41yascnik9my0glgg3w"
       ],
       "title": "Barony PT-BR.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
+        }
   ]
 }
