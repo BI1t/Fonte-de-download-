412995c9c78d1b4f34b02b83fcca2c06c389f9fb
BI1t <98274631+BI1t@users.noreply.github.com>
2025-02-25 18:59:40 -0300

    Updateeee Bit.js

--- Bit.js
+++ Bit.js
@@ -8,111 +8,110 @@
       ],
       "title": "Elden Ring.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-    },
-    {
+         },
+         {
       "fileSize": "1.35 GB",
       "uris": [
         "https://1fichier.com/?lfpsxc964sq3dcnc4m76"
       ],
       "title": "Perfect Heist 2.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-        {
+         },
+         {
       "fileSize": "624.88 MB",
       "uris": [
         "https://1fichier.com/?n41yascnik9my0glgg3w"
       ],
       "title": "Barony PT-BR.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-        {
+         },
+         {
       "fileSize": "111.58 MB",
       "uris": [
         "https://1fichier.com/?mzfwrddmsuvz8nrc9tbl"
       ],
       "title": "Stick Fight: The Game.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-        {
+         },
+         {
       "fileSize": "1.35 GB",
       "uris": [
         "https://1fichier.com/?qgtqgga9jmbgvot8ksuz"
       ],
       "title": "The Greatest Penguin Heist of All Time.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-    },
-            {
+         },
+         {
       "fileSize": "106.35 MB",
       "uris": [
         "https://1fichier.com/?xs9nv3x2qox92swiqyz6"
       ],
       "title": "Last Train Outta' Wormtown.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "1.21 GB",
       "uris": [
         "https://1fichier.com/?zhq3vtlbqggrmmx1wi0r"
       ],
       "title": "Moving Out 2.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "1.86 GB",
       "uris": [
         "https://1fichier.com/?27ppe11m2jwbd34toizg"
       ],
       "title": "Murky Divers.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "140.15 MB",
       "uris": [
         "https://1fichier.com/?krtefakiokjjns6t96y0"
       ],
       "title": "PICO PARK 2.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "144.13 MB",
       "uris": [
         "https://1fichier.com/?7m7l8s0fbj5zlkokpnbs"
       ],
       "title": "PICO PARK.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "1.56 GB",
       "uris": [
         "https://1fichier.com/?fv0mzaizn2npu8qm5c4v"
       ],
       "title": "Pummel.Party.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "116.44 MB",
       "uris": [
         "https://1fichier.com/?k4j5cyzxpqwyz1rxtg2j"
       ],
       "title": "Tricky Towers.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "943.06 MB",
       "uris": [
         "https://1fichier.com/?8oxjd851kp8adcscuiqj"
       ],
       "title": "Ultimate Chicken Horse.zip",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-                {
+         },
+         {
       "fileSize": "1.37 GB",
       "uris": [
         "https://1fichier.com/?um4fu8e1251kep8zt56s"
       ],
       "title": "Worms Revolution.zips",
       "uploadDate": "2025-02-21T18:54:06.000Z"
-        },
-    
+    }
   ]
 }
